3d5ee38cd4e1844bf4e3430e8edf01b510edc5be
Lex Li <support@lextm.com>
2025-12-26 01:15:27 -0500

    Revert a few changes and add more conditions

--- ILSpy/TreeNodes/AssemblyListTreeNode.cs
+++ ILSpy/TreeNodes/AssemblyListTreeNode.cs
@@ -19,6 +19,7 @@
 using System;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Windows;
 
 using ICSharpCode.Decompiler;
 using ICSharpCode.Decompiler.Metadata;
@@ -26,6 +27,7 @@ using ICSharpCode.Decompiler.TypeSystem;
 using ICSharpCode.Decompiler.Util;
 using ICSharpCode.ILSpyX;
 using ICSharpCode.ILSpyX.TreeView;
+using ICSharpCode.ILSpyX.TreeView.PlatformAbstractions;
 
 namespace ICSharpCode.ILSpy.TreeNodes
 {
@@ -81,6 +83,36 @@ namespace ICSharpCode.ILSpy.TreeNodes
 						throw new NotSupportedException("Invalid value for NotifyCollectionChangedAction");
 				}
 			};
+		}
+
+		public override bool CanDrop(IPlatformDragEventArgs e, int index)
+		{
+			e.Effects = XPlatDragDropEffects.Move | XPlatDragDropEffects.Copy | XPlatDragDropEffects.Link;
+			if (e.Data.GetDataPresent(AssemblyTreeNode.DataFormat))
+				return true;
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				return true;
+			e.Effects = XPlatDragDropEffects.None;
+			return false;
+		}
+
+		public override void Drop(IPlatformDragEventArgs e, int index)
+		{
+			string[] files = e.Data.GetData(AssemblyTreeNode.DataFormat) as string[];
+			if (files == null)
+				files = e.Data.GetData(DataFormats.FileDrop) as string[];
+			if (files != null)
+			{
+				var assemblies = files
+					.Where(file => file != null)
+					.Select(file => assemblyList.OpenAssembly(file))
+					.Where(asm => asm != null)
+					.Distinct()
+					.ToArray();
+				assemblyList.Move(assemblies, index);
+				var nodes = assemblies.SelectArray(AssemblyTreeModel.FindTreeNode);
+				AssemblyTreeModel.SelectNodes(nodes);
+			}
 		}
 
 		public Action<SharpTreeNode> Select = delegate { };
